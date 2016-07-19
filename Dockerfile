bb3f08c7171c045d1899927286168664d0f41204
Jens Skott <jens.skott@gmail.com>
2016-07-19 16:20:59 +0200

    It works

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM alpine:3.4
+
+RUN apk update && apk upgrade && apk --update add \
+    ruby ruby-irb ruby-rake ruby-io-console ruby-bigdecimal ruby-json ruby-bundler \
+    libstdc++ tzdata bash \
+    &&  echo 'gem: --no-document' > /etc/gemrc
+
+CMD ["irb"]
--- lib/gatekeeper.rb
+++ lib/gatekeeper.rb
@@ -1,3 +1,4 @@
+## rancher-api gem
 require 'rancher/api'
 require 'dotenv'
 
@@ -11,8 +12,45 @@ Rancher::Api.configure do |config|
   config.secret_key = ENV['SECRET_KEY']
 end
 
-project = Rancher::Api::Project.find('infraops')
-puts project
+# Function to scale up
+def scaleUp(name)
+  service = Rancher::Api::Service.find(name)
+  s = service.currentScale
+  s += 1
+  service.scale = s
+  service.save
+  puts "We now have #{service.scale.real} containers in the stack #{service.name}"
+end
 
-stack = Rancher::Api::Environment.find('1e14')
-puts stack
+def scaleDown(name)
+  service = Rancher::Api::Service.find(name)
+  # Scale the service with one container
+  s = service.currentScale
+  s -= 1
+  service.scale = s
+  service.save
+  puts "We now have #{service.scale.real} containers in the stack #{service.name}"
+end
+
+# ## find the service rest-api-test
+# service = Rancher::Api::Service.find('1s33')
+# # Scale the service with one container
+# s = service.currentScale
+# s += 1
+# service.scale = s
+# service.save
+# puts "We now have #{service.scale.real} containers in the stack #{service.name}"
+
+scaleUp('1s33')
+#
+# def every_n_seconds(n)
+# 	  loop do
+# 	    before = Time.now
+# 	    yield
+# 	    interval = n-(Time.now-before)
+# 	    sleep(interval) if interval > 0
+# 	  end
+# 	end
+# 	every_n_seconds(5) do
+# 	 puts "At the beep, the time will be #{Time.now.strftime("%X")}…beep!"
+# 	end
