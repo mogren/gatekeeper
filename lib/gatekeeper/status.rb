1970863ab03af8780b12fa8b6a42cc9559ffe0e7
Jens Skott <jens.skott@gmail.com>
2016-07-28 09:25:02 +0200

    Moved to influxDB

--- lib/gatekeeper/status.rb
+++ lib/gatekeeper/status.rb
@@ -1,5 +1,4 @@
 require_relative 'scale.rb'
-require 'dogapi'
 
 def checkContainers(id,value)
   connectRancher()
@@ -23,52 +22,12 @@ end
 
 # Check influx for data
 def checkInflux(id)
-  puts id
+  database = 'telegraf'
+  influxdb = InfluxDB::Client.new database host: "influx"
+  influxdb.query 'SELECT * FROM cpu_usage_user WHERE time > now() - 1m;'
+
+
 end
 
-# Decap?
-def checkDog (id,scaleId,scaleValue)
-  dogkey = ENV['DOGAPI_KEY']
-  dogapp = ENV['DOGAPP_KEY']
-
-  dog = Dogapi::Client.new(dogkey, dogapp)
-  # Get points from the 60 seconds
-  from = Time.now - 60
-  to = Time.now
-  #query for cpu and memory
-  cpuQuery = "avg:docker.cpu.user{io.rancher.stack.name:#{id}}"
-  cpu = dog.get_points(cpuQuery, from, to)
-  memQuery = "avg:docker.mem.in_use{io.rancher.stack.name:#{id}}"
-  mem = dog.get_points(memQuery, from, to)
-  # make them hasish
-  cpu = Hash[*cpu]
-  mem = Hash[*mem]
-  cpu = cpu['200']['series']
-  mem = mem['200']['series']
-  @cpuaverage = []
-  @memaverage = []
-
-  if mem.empty?
-    puts "No memory data available"
-  else
-    mem[0]['pointlist'].each do |p,v|
-      @memaverage.push(v)
-    end
-  end
-  if cpu.empty?
-    puts "No cpu data for #{id}"
-  else
-    cpu[0]['pointlist'].each do |p,v|
-      @cpuaverage.push(v)
-    end
-  end
-  if @cpuaverage.inject(0.0) { |sum, el| sum + el } / @cpuaverage.size > 80
-    s = Scale.new(scaleId, scaleValue)
-    s.up
-  else
-    s = Scale.new(scaleId, scaleValue)
-    s.down
-  end
-end
 ## MAKE ALGORITHM TO CHECK FOR CPU METCIS AVERAGE
 ## Query influx instead. building agents
