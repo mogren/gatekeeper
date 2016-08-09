218613e2c8e570f53abafb2033aaa06215f9a7a9
Jens Skott <jens.skott@schibsted.se>
2016-08-09 11:19:02 +0200

    Fixed status stuff

--- lib/gatekeeper/status.rb
+++ lib/gatekeeper/status.rb
@@ -23,9 +23,9 @@ end
 # Check influx for data
 def checkInflux(id)
   database = 'telegraf'
-  influxdb = InfluxDB::Client.new database host: "influx"
-  influxdb.query 'SELECT * FROM cpu_usage_user WHERE time > now() - 1m;'
-
+  influxdb = InfluxDB::Client.new database host: "influxDB"
+  c = influxdb.query 'SELECT * FROM cpu_usage_user WHERE time > now() - 1m;'
+  puts c
 
 end
 
