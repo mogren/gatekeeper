1e387c29ad3d4f70ed4768afc7fb88f52d3265d4
Claes Mogren <claes.mogren@accedo.tv>
2016-09-02 10:32:55 +0200

    More than 30%

--- lib/gatekeeper/status.rb
+++ lib/gatekeeper/status.rb
@@ -14,8 +14,8 @@ end
 def checkInflux(id)
   database = 'telegraf'
   influxdb = InfluxDB::Client.new database, host: "influxDB"
-  # TODO: Add: autoscale = 'true' AND ?
-  c = influxdb.query 'SELECT container_name, host, usage_percent FROM docker_container_cpu WITH KEY = "cpu" WHERE time > now() - 1m;'
+  # TODO: Add: autoscale = 'true' AND ? More than 30%?
+  c = influxdb.query 'SELECT container_name, host, usage_percent FROM docker_container_cpu WHERE usage_percent > 30 AND time > now() - 1m;'
   puts c
   c.length > 0
 end
